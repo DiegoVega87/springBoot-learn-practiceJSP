3b3edca5ded6eec1a04b87cba4dc87e9556a3171
DiegoVega87 <diegovega@me.com>
2024-02-15 12:30:18 +0100

    First Commit

---
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/MyFirstJavaWebappApplication.java
@@ -0,0 +1,13 @@
+package com.buildwebapps.practice.myfirstjavawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstJavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstJavaWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildwebapps/practice/myfirstjavawebapp/MyFirstJavaWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildwebapps.practice.myfirstjavawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstJavaWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
