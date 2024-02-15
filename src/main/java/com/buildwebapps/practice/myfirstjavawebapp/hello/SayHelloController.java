3d5ac30a4cc7eaa932bf57da4e2234de368a87db
DiegoVega87 <diegovega@me.com>
2024-02-15 16:43:24 +0100

    Set up app.comfig, pom.xml files to display a text in web browser via jsp

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/hello/SayHelloController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/hello/SayHelloController.java
@@ -5,17 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //This is a controller class
-@ResponseBody //This is a response body
 public class SayHelloController {
 
     //"say-hello" is the URL path --> "Hello! What are you learning today?" is the response
 
     @RequestMapping("say-hello")
+    @ResponseBody //This is a response body
     public String sayHello() {
         return "Hello! What are you learning today?";
     }
 
     @RequestMapping("say-hello-html")
+    @ResponseBody // The @ResponseBody annotation tells Spring that the return value of the method should be used as the response body of the request
     public String sayHelloHtml(){
         // Using StringBuilder to create a HTML page is not the best practice
         // but is better than using String concatenation
@@ -32,6 +33,16 @@ public class SayHelloController {
         sb.append("</h1>");
         sb.append("</body>");
         sb.append("</html>");
+
         return sb.toString();
     }
+
+    // This is a better way to create a HTML page
+    // "say-hello-jsp" --> sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello"; //This will return sayHello.jsp
+    }
+
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>My First HTML Page - JSP</title>
+    </head>
+    <body>
+        My first HTML page with body - jsp
+    </body>
+</html>
