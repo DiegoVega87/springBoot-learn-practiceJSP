1652cc6ec50f012c4b3f660218c7ccb9ef366416
DiegoVega87 <diegovega@me.com>
2024-02-19 12:40:31 +0100

    Add loginController and method to use RequestParams

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
@@ -1,14 +1,17 @@
 package com.buildwebapps.practice.myfirstjavawebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String gotToLoginPage(){
+    public String gotToLoginPage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,8 +3,6 @@
         <title>Login page</title>
     </head>
     <body>
-           Welcome to the login page!
+           Welcome to the login page ${name}!
     </body>
 </html>
-
-Exercise
