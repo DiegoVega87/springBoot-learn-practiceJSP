f08832d9cc1a69f0856893e71809c0aac6de6a80
DiegoVega87 <diegovega@me.com>
2024-02-19 13:09:08 +0100

    Add loggin to debug

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
@@ -1,17 +1,23 @@
 package com.buildwebapps.practice.myfirstjavawebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(LoginController.class); //This is a logger object to log messages to the console
+
     @RequestMapping("login")
     public String gotToLoginPage(@RequestParam String name, ModelMap model){
         model.put("name", name);
+        logger.debug("Request param is {}", name);
+        logger.info("I want this printed at info level");
         return "login";
     }
 }
