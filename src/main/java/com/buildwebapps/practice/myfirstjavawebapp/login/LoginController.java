3f3cba2e9852b8d8677c1c044983630e8e5330e0
DiegoVega87 <diegovega@me.com>
2024-02-21 12:28:42 +0100

    Add loggin at different levels

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/login/LoginController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(LoginController.class); //This is a logger object to log messages to the console
+    private Logger logger = LoggerFactory.getLogger(getClass()); //This is a logger object to log messages to the console
 
     @RequestMapping("login")
     public String gotToLoginPage(@RequestParam String name, ModelMap model){
         model.put("name", name);
-        logger.debug("Request param is {}", name);
-        logger.info("I want this printed at info level");
+        logger.debug("Request param is {}", name); // Printed only at debug level
+        logger.info("I want this printed at info level"); // Printed at info level and above
+        logger.error("I want this printed at warn level"); // Printed at error level and above
         return "login";
     }
 }
