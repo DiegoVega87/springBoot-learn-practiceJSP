bb04bc9b9a885efff106d28bc34ea4edaa4e2fcd
DiegoVega87 <diegovega@me.com>
2024-02-23 17:14:36 +0100

    Add @controller tag, todoService, constructor injection and prepared method to dump todos in .jsp

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
@@ -1,13 +1,24 @@
 package com.buildwebapps.practice.myfirstjavawebapp.todo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class TodoController {
 
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
     @RequestMapping(value="list-todos") // This is the method that will be called when the form is submitted
-    public String listAllTodos(){
+    public String listAllTodos(ModelMap model){
+        List<Todo> todos = todoService.findByUsername("Diego");
+        model.addAttribute("todos", todos); // This is the data that will be passed to the Todos page
         return "listTodos";
     }
 }
