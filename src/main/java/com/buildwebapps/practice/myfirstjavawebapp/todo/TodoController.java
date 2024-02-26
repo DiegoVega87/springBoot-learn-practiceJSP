93bad5aeaa4ac370d2cf38c746d9dede435a421e
DiegoVega87 <diegovega@me.com>
2024-02-26 19:12:51 +0100

    Add methods to handel get and post request when visiting add-todo page and adding a new todo

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
@@ -3,8 +3,11 @@ package com.buildwebapps.practice.myfirstjavawebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -22,5 +25,17 @@ public class TodoController {
         List<Todo> todos = todoService.findByUsername("Diego");
         model.addAttribute("todos", todos); // This is the data that will be passed to the Todos page
         return "listTodos";
+    }
+
+    @RequestMapping(value="add-todo", method = RequestMethod.GET) // This is the method that will be called when the form is submitted
+    public String showNewTodoPage(){
+        return "todo";
+    }
+
+    @RequestMapping(value="add-todo", method = RequestMethod.POST) // This is the method that will be called when the form is submitted
+    public String addNewTodoPage(@RequestParam String description, ModelMap model){
+        String username = (String) model.get("name");
+        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+        return "redirect:list-todos";
     }
 }
