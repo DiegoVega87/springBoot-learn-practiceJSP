0cf37576c068c5971a0e5eed9af5e8691126c414
DiegoVega87 <diegovega@me.com>
2024-02-26 11:22:13 +0100

    Adding @SessionAttributes to store values in sesion for later use

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoController.java
@@ -3,10 +3,12 @@ package com.buildwebapps.practice.myfirstjavawebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
 
 @Controller
+@SessionAttributes("name") // This is to tell Spring to keep the name in the session
 public class TodoController {
 
     private TodoService todoService;
--- src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -1,9 +1,9 @@
 <html>
     <head>
-        <title>Todos page!</title>
+        <title> Todos page!</title>
     </head>
     <body>
-        <h1>Todo List</h1>
+        <h1>Welcome ${name} to your Todo List</h1>
         <div> Your Todos are ${todos} </div>
     </body>
 </html>
