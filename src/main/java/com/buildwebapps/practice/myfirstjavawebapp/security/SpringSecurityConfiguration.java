1f430e5b90a6e39bb076b07b82497b4d0956c464
DiegoVega87 <diegovega@me.com>
2024-02-29 20:27:34 +0100

    Configure Spring Security to get H2 console working

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.buildwebapps.practice.myfirstjavawebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -33,6 +37,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // This is to create the security filter chain
+
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults()); // This is to enable the form login
+
+        http.csrf(csrf -> csrf.disable()); // This is to disable CSRF
+        http.headers(headers ->
+                headers
+                        .frameOptions(frameOptions -> frameOptions.disable())); // This is to enable H2 console
+
+        return http.build(); // This is to build the security filter chain
     }
 
 }
