d40290fae66bcfb735e1fe9cea527beeb19f2543
DiegoVega87 <diegovega@me.com>
2024-02-29 18:38:48 +0100

    Add SpringSecurityConfiguration class to create our users and passwords

---
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.buildwebapps.practice.myfirstjavawebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder()::encode)
+                .username("diego2504")
+                .password("dummy")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
