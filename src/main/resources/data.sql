84ac0f6827f370f9080e28335625d0d15a80e59c
DiegoVega87 <diegovega@me.com>
2024-03-06 13:25:00 +0100

    Add more todos in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-VALUES (1001, 'Diego', 'Get AWS Certified', CURRENT_DATE(), false);
+VALUES (1001, 'Diego', 'Get AWS Certified', CURRENT_DATE(), false),
+       (1002, 'Diego', 'Get Azure Certified', CURRENT_DATE(), false),
+       (1003, 'Diego', 'Get GCP Certified', CURRENT_DATE(), false),
+       (1004, 'Diego', 'Get DevOps', CURRENT_DATE(), false);
