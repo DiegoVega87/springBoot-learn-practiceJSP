fb07064e1e58c4404820d94c2c4e62cf3d7074aa
DiegoVega87 <diegovega@me.com>
2024-03-07 10:24:38 +0100

    changed user to lowercase letters

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-VALUES (1001, 'Diego', 'Get AWS Certified', CURRENT_DATE(), false),
-       (1002, 'Diego', 'Get Azure Certified', CURRENT_DATE(), false),
-       (1003, 'Diego', 'Get GCP Certified', CURRENT_DATE(), false),
-       (1004, 'Diego', 'Get DevOps', CURRENT_DATE(), false);
+VALUES (1001, 'diego', 'Get AWS Certified', CURRENT_DATE(), false),
+       (1002, 'diego', 'Get Azure Certified', CURRENT_DATE(), false),
+       (1003, 'diego', 'Get GCP Certified', CURRENT_DATE(), false),
+       (1004, 'diego', 'Get DevOps', CURRENT_DATE(), false);
