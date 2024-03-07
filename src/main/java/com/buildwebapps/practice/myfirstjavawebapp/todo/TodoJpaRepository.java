04c9e88e2bdadccda65612d6ec2933dbd75854ee
DiegoVega87 <diegovega@me.com>
2024-03-07 10:27:30 +0100

    Add method to find by user

--- src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoJpaRepository.java
+++ src/main/java/com/buildwebapps/practice/myfirstjavawebapp/todo/TodoJpaRepository.java
@@ -2,6 +2,10 @@ package com.buildwebapps.practice.myfirstjavawebapp.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoJpaRepository extends JpaRepository<Todo, Integer> {
+
+    List<Todo> findByUsername(String username);
 
 }
